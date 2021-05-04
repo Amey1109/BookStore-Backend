ca30aee0f4880663cc6a81df3b11a2a7828d8959
Amey1109 <bhogaonkaramey@gmail.com>
2021-05-04 19:16:13 +0530

    Added RegisterCustomer,RegisterAdmin, UpdateCustomer API's

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path, include
 from accounts import views
 urlpatterns = [
-    path('generate_otp/', views.generate_otp),
+    path('generate_otp/', views.generate_otp),  # Generates the OTP
+    path('check_otp/', views.check_otp),  # Checks the OTP Generated OTP
+
+    # Register Normal Customer
+    path('register_customer/', views.register_customer),
+
+    # Register Admin with Admin rights
+    path('register_admin/', views.register_admin),
     
-    path('check_otp/', views.check_otp),
+    # Updates the registered customers
+    path('update_customer/', views.update_customer)
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,15 @@ from rest_framework.decorators import api_view
 # Generates the OTP and sends to the mobile number
 @api_view(['POST'])
 def generate_otp(request):
-    number = request.data['number']
+    if 'number' in request.data:  # Checks whether the nuber is present in the request body
+        # checks weather the number present in the request body is of 10-Digit
+        if len(request.data['number']) == 10:
+            number = request.data['number']
+        else:
+            return Response({"Error": "Mobile Number should be of 10-digits", "isOTPSent": False})
+    else:
+        return Response({"Error": "Expected phone number in request body not present", "isOTPSent": False})
+
     generatedOTP = generatingOTP(number)  # Generates the 4-Digit OTP
     if generatedOTP:
         data = OTP(phone_number=number, otp=generatedOTP)
@@ -23,21 +31,24 @@ def generate_otp(request):
     else:
         return Response({"isOTPSent": False})
 
+# Checks the OTP generated for corresponding Phone Number
+
 
 @api_view(['PUT'])
 def check_otp(request):
-
     # validations for Checking if the request.data has what we need
     if 'number' in request.data:
-        number = request.data['number']
+        if len(request.data['number']) == 10:
+            number = request.data['number']
+        else:
+            return Response({"Error": "Mobile Number should be of 10-digits", "checkStatus": False})
     else:
-        return Response({"Error": "Expected phone number in request body not present"})
+        return Response({"Error": "Expected phone number in request body not present", "checkStatus": False})
 
     if 'otp' in request.data:
         otp = request.data['otp']
     else:
-        return Response({"Error": "Expected OTP in request body not present"})
-
+        return Response({"Error": "Expected OTP in request body not present", "checkStatus": False})
 
     generatedOTP = OTP.objects.filter(
         phone_number=number).values_list('otp')
@@ -53,3 +64,161 @@ def check_otp(request):
         return Response({"checkStatus": True})
     else:
         return Response({"checkStatus": False})
+
+# Register Normal User
+
+
+@api_view(['POST'])
+def register_customer(request):
+    if 'username' in request.data:
+        user_name = request.data['username']
+    else:
+        return Response({"Error": "Username Not Provided"})
+
+    if 'fname' in request.data:
+        fname = request.data['fname']
+    else:
+        return Response({"Error": "First Name not Provided"})
+
+    if 'lname' in request.data:
+        lname = request.data['lname']
+    else:
+        return Response({"Error": "Last Name not Provided"})
+
+    if 'email' in request.data:
+        email = request.data['email']
+    else:
+        return Response({"Error": "Email Not Provided"})
+
+    if 'number' in request.data:
+        phone_number = request.data['number']
+    else:
+        return Response({"Error": "Contact Number Not Provided"})
+
+    if 'password' in request.data:
+        password = request.data['password']
+    else:
+        return Response({"Error": "Password Not Provided"})
+
+    customer = Customer(email=email)
+    customer.username = user_name
+    customer.phone_number = phone_number
+    customer.first_name = fname
+    customer.last_name = lname
+    customer.set_password(password)
+
+    try:
+        customer.save()
+        try:
+            otp_clutter = OTP.objects.get(phone_number=phone_number)
+
+            if otp_clutter.is_verfied:
+                otp_clutter.delete()
+            else:
+                return Response({"Error": "Mobile Number Not Verified"})
+        except OTP.DoesNotExist as e:
+            return Response({"Error": "Mobile Does Not Exists"})
+
+        return Response({"registerStatus": True, "IntegrityError": False})
+    except IntegrityError as e:
+        return Response({"registerStatus": False, "IntegrityError": True})
+
+
+# Registers Admin
+@api_view(['POST'])
+def register_admin(request):
+    if 'username' in request.data:
+        user_name = request.data['username']
+    else:
+        return Response({"Error": "Username Not Provided"})
+
+    if 'fname' in request.data:
+        fname = request.data['fname']
+    else:
+        return Response({"Error": "First Name not Provided"})
+
+    if 'lname' in request.data:
+        lname = request.data['lname']
+    else:
+        return Response({"Error": "Last Name not Provided"})
+
+    if 'email' in request.data:
+        email = request.data['email']
+    else:
+        return Response({"Error": "Email Not Provided"})
+
+    if 'number' in request.data:
+        phone_number = request.data['number']
+    else:
+        return Response({"Error": "Contact Number Not Provided"})
+
+    if 'password' in request.data:
+        password = request.data['password']
+    else:
+        return Response({"Error": "Password Not Provided"})
+
+    admin = Customer.objects.create_superuser(
+        username=user_name, email=email, first_name=fname, last_name=lname, phone_number=phone_number, password=password)
+    try:
+        admin.save()
+        try:
+            otp_clutter = OTP.objects.get(phone_number=phone_number)
+
+            if otp_clutter.is_verfied:
+                otp_clutter.delete()
+            else:
+                return Response({"Error": "Mobile Number Not Verified"})
+        except OTP.DoesNotExist as e:
+            return Response({"Error": "Mobile Does Not Exists"})
+
+        return Response({"registerStatus": True, "IntegrityError": False})
+    except IntegrityError as e:
+        return Response({"registerStatus": False, "IntegrityError": True})
+
+
+@api_view(['PUT'])
+def update_customer(request):
+    id = request.data['id']
+    if 'username' in request.data:
+        new_user_name = request.data['username']
+    else:
+        return Response({"Error": "Username Not Provided"})
+
+    if 'fname' in request.data:
+        new_fname = request.data['fname']
+    else:
+        return Response({"Error": "First Name not Provided"})
+
+    if 'lname' in request.data:
+        new_lname = request.data['lname']
+    else:
+        return Response({"Error": "Last Name not Provided"})
+
+    if 'email' in request.data:
+        email = request.data['email']
+    else:
+        return Response({"Error": "Email Not Provided"})
+
+    if 'number' in request.data:
+        new_phone_number = request.data['number']
+    else:
+        return Response({"Error": "Contact Number Not Provided"})
+
+    if 'password' in request.data:
+        new_password = request.data['password']
+    else:
+        return Response({"Error": "Password Not Provided"})
+
+    try:
+        customer = Customer.objects.get(id=id)
+        customer.username = new_user_name
+        customer.phone_number = new_phone_number
+        customer.first_name = new_fname
+        customer.last_name = new_lname
+        customer.set_password(new_password)
+        customer.save()
+        return Response({"status": True})
+
+    except Customer.DoesNotExist as e:
+
+        return Response({"status": False, "Error ": "Customer does not exist"})
